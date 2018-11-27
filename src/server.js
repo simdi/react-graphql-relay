7dfea24ca8001fd7c632f03c23b3d67fe11d183c
Chisimdi Damian Ezeanieto <simdi4iverson@yahoo.co.uk>
2018-11-27 20:22:44 +0100

    Added babel and created a public folder.

---
+++ src/server.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello from express!');
+});
+
+app.listen(port , _=> console.log(`Server started on port ${port}`));
