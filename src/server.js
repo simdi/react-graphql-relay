28b78666944be9db31e41b6bbaf35e1d4941ed17
Chisimdi Damian Ezeanieto <simdi4iverson@yahoo.co.uk>
2018-11-28 08:58:53 +0100

    Added a code to serve from the public folder.

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use('/public', express.static(path.resolve(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-    res.send('Hello from express!');
+    res.sendFile(`${__dirname}/public/index.html`);
 });
 
 app.listen(port , _=> console.log(`Server started on port ${port}`));
